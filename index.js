65f12e4ee0060e36fb2682a0d5ddc924546db0ac
Sergey Mikerin <91702021+smikerin@users.noreply.github.com>
2022-03-24 07:24:29 +0000

    done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+var Calculator = require("./Calculator.js");//.js is optional
+var calc = new Calculator();
+let addResult = calc.add(2,1);
+let divideResult = calc.divide(8,0);
+let output = `2+1=${addResult}, 
+8/0=${divideResult}`;
+console.log(output);
+
